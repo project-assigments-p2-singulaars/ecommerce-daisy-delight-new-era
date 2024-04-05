770bf7ebaf89e282cf03ed85ac044a69d60547cb
santiiii12 <santi.agudo12@gmail.com>
2024-04-05 11:39:17 +0200

    update json

--- source/cards.js
+++ source/cards.js
@@ -1,17 +1,56 @@
-let  apiUrlFlowers ="http://localhost:3000/plants";
-let  apiUrlFestivities ="http://localhost:3000/festivities";
-let  apiUrlBirthday ="http://localhost:3000/birthday";
+let apiUrlFlowers = "http://localhost:3000/";
 
-async function callApi(url){
-    const response = await fetch(url).then(response => response.json());
-    response.map((index) => {
-        console.log(index);
-    })
-    // const productos = await response.json();
-    // console.log(productos);
+function callApi(apiUrlFlowers) {
+    try {
+        const response = fetch(apiUrlFlowers);
+        const data = response.json();
+        data.map((DaisyDelight) => {
+            console.log(DaisyDelight);
+        });
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+    }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const searchButton = document.getElementById('searchButton');
 
-callApi(apiUrlFlowers);
-callApi(apiUrlFestivities);
-callApi(apiUrlBirthday);
+    searchButton.addEventListener('click', function() {
+        const userInput = document.getElementById('search-input').value;
+        filterPlants(userInput);
+    });
+});
+
+async function filterPlants(userInput) {
+    try {
+        const response = await fetch(apiUrlFlowers);
+        const DaisyDelight= await response.json();
+        plantList.innerHTML = '';
+
+        if (DaisyDelight.length > 0) {
+            const filteredPlants = DaisyDelight.filter(DaisyDelight => DaisyDelight.name.includes(userInput));
+
+            filteredPlants.forEach(DaisyDelight => {
+                const plantItem = document.createElement('div');
+                plantItem.classList.add("plantCard");
+
+                plantItem.innerHTML = `
+                    <h2>${DaisyDelight.name}</h2>
+                    <p><strong>Descripción:</strong> ${DaisyDelight.description}</p>
+                    <img src="${DaisyDelight.photo}" alt="${DaisyDelight.name}" style="max-width: 200px;">
+                    <p><strong>Precios:</strong> ${DaisyDelight.price1}, ${DaisyDelight.price2}, ${DaisyDelight.price3}</p>
+                    <hr>
+                `;
+                plantList.appendChild(plantItem);
+            });
+        } else {
+            const noResults = document.createElement('p');
+            noResults.textContent = 'No se encontraron plantas.';
+            plantList.appendChild(noResults);
+        }
+    } catch (error) {
+        console.error('Error al filtrar plantas:', error);
+    }
+}
+
+callApi='http://localhost:3000/'

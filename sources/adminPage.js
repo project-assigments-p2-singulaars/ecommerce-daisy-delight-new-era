c9918a0a0272c531abc24ec1100d359f09c08b4a
SaraJoLo <sarajorja.27@gmail.com>
2024-04-10 13:23:08 +0200

    update feature/shoppingCart

--- sources/adminPage.js
+++ sources/adminPage.js
@@ -3,19 +3,39 @@ let formulario = document.getElementById('formularioAdmin');
 formulario.addEventListener('submit', event =>{
     event.preventDefault();
     console.log('producto enviado')
+    console.log(event.target.querySelectorAll("input"))
 
-    let datos = new FormData(formulario)
-    console.log(datos)
-    console.log(datos.get('namePro'))
-    console.log(datos.get('imgPro'))
-    console.log(datos.get('descriptionPro'))
-    console.log(datos.get('pricePro1'))
-    console.log(datos.get('pricePro2'))
-    console.log(datos.get('pricePro3'))
+ const nameValue = document.getElementById("namePro");
+ const imgProValue = document.getElementById("imgPro");
+ const descriptionProValue = document.getElementById("descriptionPro");
+ const pricePro1Value = document.getElementById("pricePro1");
+ const pricePro2Value = document.getElementById("pricePro2");
+ const pricePro3Value = document.getElementById("pricePro3");
+ const categoryProdValue = document.getElementById("categoryProd");
+
+    const datos = {
+        name: nameValue.value,
+        img: imgProValue.value,
+        description: descriptionProValue.value,
+        price1: pricePro1Value.value,
+        price2: pricePro2Value.value,
+        price3: pricePro3Value.value,
+        categoryProd: categoryProdValue.value,
+    }
     
-    fetch(formulario, {
-        method: 'POST',
-        headers: datos
-    })
+    console.log(datos)
+    
 
+    fetch('http://localhost:3000/plants', {
+        method: 'POST',
+        body: JSON.stringify(datos),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        })
+          .then((response) => response.json())
+          .then((json) => console.log(json));
 })
+    fetch('http://localhost:3000/plants', {
+      method: 'DELETE',
+    });
